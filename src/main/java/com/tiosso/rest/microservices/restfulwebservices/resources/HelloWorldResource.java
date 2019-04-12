4c7edce55d60396a841cbc306797a31b22487844
Guilherme Tiosso <guilherme.tiosso@gmail.com>
2019-04-12 17:48:25 -0300

    Simple Rest - Create HelloWorld Resources

--- src/main/java/com/tiosso/rest/microservices/restfulwebservices/resources/HelloWorldResource.java
+++ src/main/java/com/tiosso/rest/microservices/restfulwebservices/resources/HelloWorldResource.java
@@ -1,31 +1,32 @@
-package com.tiosso.rest.microservices.restfulwebservices;
+package com.tiosso.rest.microservices.restfulwebservices.resources;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tiosso.rest.microservices.restfulwebservices.domain.HelloWorldBean;
 
-// Definindo um Controller
+//Definindo um Controller
 @RestController
 
-public class HelloWorldController {
-	
+// Definindo uma rota padrão -  recurso (URI) base
+@RequestMapping(path = "/hello-world")
+
+public class HelloWorldResource {
+
 	// Definindo Metodo HTTP, rota URI e Retornando uma string simples
-	@GetMapping(path = "/hello-world")
+	@GetMapping
 	public ResponseEntity<String> helloWorld() {
 		return ResponseEntity.ok().body("Hello World !");
 		
 	}
 	
 	// Definindo Metodo HTTP, rota URI e Retornando um Bean (Objeto)
-	@GetMapping(path = "/hello-world-bean")
+	@GetMapping(path = "/bean")
 	public ResponseEntity<HelloWorldBean> helloWorldBean() {
 		HelloWorldBean obj = new HelloWorldBean("Hello World !");
 		return ResponseEntity.ok().body(obj);
 		
 	}
-	
-	
-
 }
